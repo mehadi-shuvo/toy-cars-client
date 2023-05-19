e332995d4997d26050fa716b2177cea258bfb9d3
mehadi-shuvo <mehadihasanshuvo88@gamil.com>
2023-05-19 23:02:59 +0600

    add firebase and register auth done

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,23 +1,45 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider/AuthProvider";
 
 
 const Register = () => {
+    const {createUser, addNameAndPhoto} = useContext(AuthContext)
 
-    const handelRegister = event =>{
-        event.preventDefault()
+    const handleRegister = (event)=>{
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        createUser(email,password)
+        .then(result=>{
+            const user = result.user;
+            addNameAndPhoto(user, name, photo)
+                .then().catch(error=>{
+                    console.log(error.message);
+                })
+            console.log(user)
+            event.form.reset()
+        })
+        .catch(error=>{
+            console.log(error.message);
+        })
+        
     }
     return (
         <div className='w-2/5 mx-auto bg-red-500 mt-10 rounded-lg my-20'>
-            <form onSubmit={handelRegister} className='flex gap-7 flex-col p-7'>
+            <form onSubmit={handleRegister} className='flex gap-7 flex-col p-7'>
                 <h4 className='text-center text-3xl font-extrabold text-white'>Register Now</h4>
-                <input className='p-3 rounded-lg text-center' type="text"
-                    placeholder='Your Name' />
-                <input className='p-3 rounded-lg text-center' type="text"
+                <input className='p-3 rounded-lg text-center' name="name" type="text"
+                    placeholder='Your Name' required />
+                <input className='p-3 rounded-lg text-center' name="photo" type="text"
                     placeholder='Your Photo URL' />
-                <input className='p-3 rounded-lg text-center' type="email"
-                    placeholder='Your Email' />
-                <input className='p-3 rounded-lg text-center' type="password"
-                    placeholder='Your Password' />
+                <input className='p-3 rounded-lg text-center' name="email" type="email"
+                    placeholder='Your Email' required />
+                <input className='p-3 rounded-lg text-center' name="password" type="password"
+                    placeholder='Your Password' required />
                 <button type="submit" className='py-3 rounded-lg bg-red-700 hover:bg-red-800 font-semibold text-2xl text-white'>Register</button>
                 <div>
                     <div className="divider text-white">OR</div>
