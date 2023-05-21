ba9bb96c2f7f32618450603f56cf33356907fca3
mehadi-shuvo <mehadihasanshuvo88@gamil.com>
2023-05-21 09:27:53 +0600

    search option in all toy page

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -17,11 +17,22 @@ const AllToys = () => {
     const handleLoadMore = () => {
         setShowFullData(true);
     };
-
+    const handleSearch = event =>{
+        event.preventDefault()
+        const search = event.target.search.value;
+        fetch(`http://localhost:3000/search?name=${search}`)
+        .then(res=>res.json())
+        .then(data=>setToys(data))
+        .catch(error=>console.log(error))
+    }
 
     return (
         <div className="w-4/5 mx-auto">
             <div>
+                <form onSubmit={handleSearch} className="flex justify-center items-center w-1/2 mx-auto gap-6 mt-10">
+                    <input name="search" type="text" placeholder="Type here" className="input w-full bg-red-50" />
+                    <button className="py-2 rounded-lg bg-red-500 hover:bg-red-800 font-semibold text-lg uppercase px-4 text-white">Search</button>
+                </form>
                 <div className="overflow-x-auto w-full my-12">
                     <table className="table w-full">
                         <thead >
@@ -46,10 +57,10 @@ const AllToys = () => {
             </div>
 
             <div className="flex justify-center my-8">
-            {
-                showFullData? ''
-                : <button className="w-1/4 mx-auto py-3 rounded-lg bg-red-500 hover:bg-red-800 font-semibold text-lg uppercase px-4 text-white" onClick={handleLoadMore}>load more</button>
-            }
+                {
+                    showFullData ? ''
+                        : <button className="w-1/4 mx-auto py-3 rounded-lg bg-red-500 hover:bg-red-800 font-semibold text-lg uppercase px-4 text-white" onClick={handleLoadMore}>load more</button>
+                }
             </div>
         </div>
     );
