390757bbb72d4165d8ee01ff59efb95e09632aea
mehadi-shuvo <mehadihasanshuvo88@gamil.com>
2023-05-21 10:55:58 +0600

    using aos in home page

--- src/pages/Home/Home/Featured.jsx
+++ src/pages/Home/Home/Featured.jsx
@@ -1,12 +1,21 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 import { Link } from "react-router-dom";
 
 
 const Featured = () => {
+    useEffect(() => {
+        AOS.init();
+      }, []);
     return (
         <div className="my-20">
             <h4 className="text-center text-red-500 font-bold text-5xl mb-5">Our Featured Cars</h4>
             <div className="grid md:grid-cols-3 w-4/5 mx-auto gap-5">
-                <div className="card image-full">
+                <div data-aos="zoom-in"
+                    data-aos-easing="linear"
+                    data-aos-duration="500"
+                    className="card image-full">
                     <figure><img src='https://i.postimg.cc/m2fpwwVW/jed-owen-n-Hnt-I-z-Oh-Y4-unsplash.jpg' className="opacity-100" alt="Shoes" /></figure>
                     <div className="p-8 text-white z-20 flex flex-col justify-end">
                         <h2 className="card-title text-3xl font-bold text-white">Lights Dumper Truck Toy</h2>
@@ -15,7 +24,10 @@ const Featured = () => {
                         </div>
                     </div>
                 </div>
-                <div className="card image-full">
+                <div data-aos="zoom-in"
+                    data-aos-easing="linear"
+                    data-aos-duration="500"
+                    className="card image-full">
                     <figure><img src='https://i.postimg.cc/Bn3yqg1B/gabriel-vasiliu-b-Ha-LQz5ko-Us-unsplash.jpg' className="opacity-100" alt="Shoes" /></figure>
                     <div className="p-8 text-white z-20 flex flex-col justify-end">
                         <h2 className="card-title text-3xl font-bold text-white">Heavy Duty Push n Go Vehicles </h2>
@@ -24,7 +36,10 @@ const Featured = () => {
                         </div>
                     </div>
                 </div>
-                <div className="card image-full">
+                <div data-aos="zoom-in"
+                    data-aos-easing="linear"
+                    data-aos-duration="500"
+                    className="card image-full">
                     <figure><img src='https://i.postimg.cc/J7BdZgNQ/gabriel-vasiliu-Niu-BAomdr-T4-unsplash.jpg' className="opacity-100" alt="Shoes" /></figure>
                     <div className="p-8 text-white z-20 flex flex-col justify-end">
                         <h2 className="card-title text-3xl font-bold text-white">High Speed Cross-country </h2>
